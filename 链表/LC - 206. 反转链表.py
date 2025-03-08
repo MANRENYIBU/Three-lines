96271191ba89c678671057395b854c0fc9f7e4cf
TKine <158488741+TKineCC@users.noreply.github.com>
2025-03-08 17:21:55 +0800

    更新 链表 题目

---
+++ 链表/LC - 206. 反转链表.py
@@ -0,0 +1,20 @@
+# 题目来源：https://leetcode.cn/problems/reverse-linked-list/description/
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pre = None
+        cur = head
+        while cur:
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+        return pre
