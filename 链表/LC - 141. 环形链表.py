fbd1b719083bc71aff96309c46da7e2e4a4a543d
TKine <158488741+TKineCC@users.noreply.github.com>
2025-03-12 11:33:02 +0800

    更新 链表 题目

---
+++ 链表/LC - 141. 环形链表.py
@@ -0,0 +1,19 @@
+# 题目来源：https://leetcode.cn/problems/linked-list-cycle/description/
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast is slow:
+                return True
+        return False
